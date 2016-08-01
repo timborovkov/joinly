b377cb40406448d1138f0636f05767b6051a9873
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-08-01 16:22:51 +0300

    add socket io

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/site'));
 
-app.get('/', function(req, res) {
-  res.send('Hello')
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 app.listen(app.get('port'), function() {
