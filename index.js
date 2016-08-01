bf103d7bd5d26c85227768809f952f2634b39a0c
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-08-01 16:42:27 +0300

    fix socket io

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/site'));
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('disconnect', function () {
+    console.log("user disconnected");
+  });
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
