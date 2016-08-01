afea171633659395cb3a79f5d8feae508a0ff724
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-08-01 16:27:58 +0300

    remove  socket io

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/site'));
-
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
