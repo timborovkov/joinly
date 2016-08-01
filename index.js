2a3e8408fa8f163c589c418cc10eb7f4c0f62579
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-08-01 15:43:56 +0300

    PORT change to 3000

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+var port = 3000;
 
 app.get('/', function (req, res) {
  res.send('Hello World!');
 });
 
-app.listen(PORT, function () {
- console.log('Example app listening on port ' + PORT);
+app.listen(port, function () {
+ console.log('Example app listening on port ' + port);
 });
