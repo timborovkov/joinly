68b593e84e032b17f60c9c88d16013e690e4db03
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-08-03 00:57:15 +0300

    update

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ var path = require('path');
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var firebase = require('firebase');
+var request = require('request');
 
-server.listen(process.env.PORT || 5000);
+server.listen(process.env.PORT || 3000);
 
 app.use("/", express.static(path.join(__dirname, 'site')));
 
@@ -19,6 +20,14 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 
+//Sign in to Firebase server user account
+//This account has access to everything in database
+firebase.auth().signInWithEmailAndPassword('server@joinly.org', 'yD4e4SmTWnv7Gjc').catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+});
+
 io.on('connection', function (socket) {
   console.log('user has been connected');
   socket.on('leave', function(data){
@@ -26,7 +35,7 @@ io.on('connection', function (socket) {
     //data = {uid, queue}
 
     //1. Remove uid from queue in the database
-    firebase.database().ref('queue/'+data.queue+'/queue').on('value', function(snapshot) {
+    database.ref('queue/'+data.queue+'/queue').on('value', function(snapshot) {
       var queue = snapshot.val();
       var newQueue = {};
       for (var userInQueue in queue) {
@@ -44,6 +53,7 @@ io.on('connection', function (socket) {
             }
           }else{
             //Do not add this to new queue, cause this is the user which wanted to leave
+            database.ref('user/'+userInQueue+'/ongoingQueue').set(null);
           }
         }
       }
@@ -62,7 +72,7 @@ io.on('connection', function (socket) {
     //Queue manager clicked 'Next customer' button
     //data = {queue}
     //1. Revove first user from the queue
-    firebase.database().ref('queue/'+data.queue+'/queue').on('value', function(snapshot) {
+    database.ref('queue/'+data.queue+'/queue').on('value', function(snapshot) {
       var queue = snapshot.val();
       var newQueue = {};
       for (var userInQueue in queue) {
@@ -70,11 +80,34 @@ io.on('connection', function (socket) {
           if (queue[userInQueue] == 1) {
             //This user must removed from the queue
             socket.emit(data.queue, { uid: userInQueue, do: 'removeFromQueue'});
+            database.ref('user/'+userInQueue+'/ongoingQueue').set(null);
           }else if (queue[userInQueue] == 0) {
             //This is 0 user (it must stay)
             newQueue[userInQueue] = 0;
           }else{
             newQueue[userInQueue] = queue[userInQueue] - 1;
+
+            if((queue[userInQueue] -1) == 2){
+              //If next in queue
+              //Get user notification token
+              database.ref('user/'+userInQueue+'/notificationToken').on('value', function(snapshot){
+                var notificationToken = snapshot.val();
+                //Send notification
+                request.post('https://push.ionic.io/api/v1/push', {
+                  form:{
+                    tokens:[
+                      notificationToken
+                    ],
+                    "notification": {
+                      "alert":"You are next in the queue!"
+                    }
+                  }
+                });
+              });
+            }else if(){
+              //It is your time
+              //Send notification
+            }
             socket.emit(data.queue, { uid: userInQueue, do: 'moveFroward'});
           }
         }
@@ -86,4 +119,42 @@ io.on('connection', function (socket) {
       database.ref('queue/'+data.queue+'/queue').set(newQueue);
     });
   })
+});
+
+app.get('/messangerCallback', function(req, res){
+  res.send('hello');
+});
+
+app.get('/notify', function(req, res) {
+  var uid = req.query.uid;
+
+  database.ref('user/'+uid+'/notificationToken').on('value', function(snapshot){
+    var notificationToken = snapshot.val();
+    //Send notification
+    var reqOptions = {
+      uri: 'https://push.ionic.io/api/v1/push',
+      method: 'post',
+      body: {
+        tokens:[
+          notificationToken
+        ],
+        "notification": {
+          "alert":"You are next in the queue!"
+        }
+      },
+      headers: {
+        "Content-Type": "application/json",
+        "X-Ionic-Application-Id": "345820ce",
+        "Authorization": "".replace("\n", "")
+      }
+    }
+    request(reqOptions, function(error, response, body){
+      if (!error && response.statusCode == 200) {
+        console.log(body);
+      }else{
+        console.log(response);
+        console.error(error);
+      }
+    });
+  });
 });
