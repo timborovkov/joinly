e8b2e00db41aa9eb651a147f6ff76b83f3aa2893
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-08-03 02:49:28 +0300

    update

--- index.js
+++ index.js
@@ -159,7 +159,8 @@ app.get('/webhook', function(req, res){
 app.post('/webhook', function(req, res){
   var data = req.body;
   console.log("--------WEBHOOK--------");
-  console.log(data);
+  console.log(req);
+  res.sendStatus(200);
 })
 
 function notify(uid, text){
