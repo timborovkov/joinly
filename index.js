e449ad8d3cd0f7f494316f11e19727a5ebbf06d1
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-08-03 01:35:20 +0300

    update

--- index.js
+++ index.js
@@ -130,26 +130,104 @@ io.on('connection', function (socket) {
   })
 });
 
-app.get('/setup', function(req, res){
-  //Sign in to Firebase server user account
-  //This account has access to everything in database
-  firebase.auth().signInWithEmailAndPassword('server@joinly.org', 'yD4e4SmTWnv7Gjc').catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-  });
-});
+app.post('/webhook', function (req, res) {
+  var data = req.body;
+  if (data.object == 'page') {
+    data.entry.forEach(function(pageEntry) {
+      var pageID = pageEntry.id;
+      var timeOfEvent = pageEntry.time;
 
-app.get('/webhook', function(req, res){
-  if (req.query['hub.mode'] === 'subscribe' &&
-    req.query['hub.verify_token'] === "thisIsEpic") {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);
+      pageEntry.messaging.forEach(function(messagingEvent) {
+        if (messagingEvent.optin) {
+          receivedAuthentication(messagingEvent);
+        } else if (messagingEvent.message) {
+          receivedMessage(messagingEvent);
+        } else if (messagingEvent.delivery) {
+          receivedDeliveryConfirmation(messagingEvent);
+        } else if (messagingEvent.postback) {
+          receivedPostback(messagingEvent);
+        } else {
+          console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+        }
+      });
+    });
+
+    res.sendStatus(200);
   }
 });
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:",
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+
+  // You may get a text or attachment but not both
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText) {
+    switch (messageText) {
+      case 'image':
+        sendImageMessage(senderID);
+        break;
+
+      case 'button':
+        sendButtonMessage(senderID);
+        break;
+
+      case 'generic':
+        sendGenericMessage(senderID);
+        break;
+
+      case 'receipt':
+        sendReceiptMessage(senderID);
+        break;
+
+      default:
+        sendTextMessage(senderID, messageText);
+    }
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+}
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+
+  callSendAPI(messageData);
+}
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: "EAAG3xPE4ZA6MBAI0sQWNdS2jpErB0LelEpne1ZCslwIfClmGZCPNsptprrwdV3iSsGCBHvctBxocrs0HfLy8eCsUXMgfThZCZByIC6dgUQPzFlmHFKBJUaVlKkpTaOf7ugoJq5Jyzm8hVBZBkkhSZBrfJQntxS2T4O2DzrIvdKoawZDZD" },
+    method: 'POST',
+    json: messageData
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s",
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });
+}
 
 function notify(uid, text){
   //TODO
