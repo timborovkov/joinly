99e60106ba68520de74d94ced34aa723133796ad
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-08-03 01:06:07 +0300

    update

--- index.js
+++ index.js
@@ -122,13 +122,7 @@ io.on('connection', function (socket) {
 });
 
 app.get('/webhook', function(req, res){
-  if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === "helloServer") {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);
-  }
+  res.send(req.query['hub.challenge']);
 });
 
 app.get('/notify', function(req, res) {
